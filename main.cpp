d83bf527066ebb8156812f2c440bd48456f7da02
Andrés Felipe <fmendez1212@gmail.com>
2021-07-31 22:35:27 -0500

    cleanup

--- main.cpp
+++ main.cpp
@@ -1,17 +1,5 @@
-// SDL2OPENGL.cpp : Defines the entry point for the application.
-//
-
-#include "framework.h"
-#include "Resource.h"
-
-#include <GL/gl.h>
-#include <stdio.h>
 #include "platform.h"
 #include "renderer.h"
-
-#define MAX_LOADSTRING 100
-
-
 
 Platform gPlatform;
 Renderer gRenderer;
@@ -21,21 +9,12 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
                      _In_ LPWSTR    lpCmdLine,
                      _In_ int       nCmdShow)
 {
-    UNREFERENCED_PARAMETER(hPrevInstance);
-    UNREFERENCED_PARAMETER(lpCmdLine);
-
     gPlatform.startUp();
-
-    /// run
 
     while (gPlatform.isRunning) {
 		gPlatform.runLoop();
 
     }
 
-    /// shutdown
-    
     gPlatform.shutDown();
-
-
 }
